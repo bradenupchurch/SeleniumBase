1f174b87795bd5c4663077a2bdbe9e847e03a042
Brad <56045105+bradenupchurch@users.noreply.github.com>
2022-04-21 16:27:22 -0600

    GCP VM install
    Adds paths, Python and PIP dependencies.

---
+++ SeliniumVMinstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#my installation of Selinium install on GCP VM
+
+git clone https://github.com/seleniumbase/SeleniumBase.git
+sudo apt -y purge python-pip
+sudo python -m pip uninstall pip
+sudo apt -y install python-pip
+pip install --upgrade pip
+echo "source `which virtualenvwrapper.sh`" > ~/.bashrc
+echo "export PATH=\"${HOME}/.local/bin:$PATH\"" >>"${HOME}"/.bashrc
+python3 -m pip install setuptools-rust
+python3 -m pip install virtualenvwrapper --force-reinstall
+export WORKON_HOME=$HOME/.virtualenvs
+source `which virtualenvwrapper.sh`
+source virtualenv_install.sh
+pip install -U seleniumbase
